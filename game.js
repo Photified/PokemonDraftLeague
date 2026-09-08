b2abf6b7e2ade26c2b93f650e6002a9cc04d514f
Photified <justinmainphoto@gmail.com>
2026-09-08 13:35:16 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -1342,7 +1342,7 @@ async function init(){
         window.location.reload();
       });
 
-      navigator.serviceWorker.register('./sw.js?v=1.9.0',{updateViaCache:'none'})
+      navigator.serviceWorker.register('./sw.js?v=1.10.0',{updateViaCache:'none'})
         .then(reg=>{
           const activateNow=worker=>{
             if(worker) worker.postMessage({type:'SKIP_WAITING'});
